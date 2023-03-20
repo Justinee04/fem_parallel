7ba04cd5a05aa0243df45e9e5f91130addf253ea
Justine Banc <justineh04@gmail.com>
2023-03-20 19:40:55 +0100

    Calcul de b aux interfaces et conditions limites

--- splitter_parallel.py
+++ splitter_parallel.py
@@ -4,6 +4,7 @@ from splitter import *
 from mpi4py import MPI
 import visu_split_mesh
 
+comm = MPI.COMM_WORLD
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 
@@ -16,7 +17,8 @@ def get_nodes_subdomain(sub_domain, rank):
         s = s | set(msh.elt2verts[glob_el].tolist())
     print(s)
     connec_mat = np.array(list(s))
-    print(connec_mat)
+    print("connec_mat", connec_mat)
+    print(np.in1d(connec_mat, np.array([1, 3, 5])).nonzero()[0])
     return s
 
 
@@ -39,6 +41,8 @@ if __name__ == '__main__':
     # Récupération des noeuds communs entre les sous-domaines
     interf_one2one = [get_nodes_subdomain(splitted_elt[rank], rank) & get_nodes_subdomain(splitted_elt[k], k) for k in
                       range(nb_domains) if k != rank]
+    print("nodes" ,interf_one2one[0])
+
     interfNodes = list(set().union(*interf_one2one))
 
     # Elements globaux du sous-domaine de rang rank
@@ -46,11 +50,13 @@ if __name__ == '__main__':
     # Sommets globaux du sous-domaine de rang rank
     verts = list(set(msh.elt2verts[list(el_subdomain)].flatten()))
     verts.sort()
+    print("verts", verts)
     # L'index est le numero local du sommet et la valeur est le numero global du sommet
     loc2glob_array = np.array(verts)
+    # Noeud a l'interface en numerotation locale !!
+    interf_loc = np.in1d(verts, list(interf_one2one[0])).nonzero()[0]
+    print("loc ========", interf_loc)
 
-
-    # print('loc2glob ===============================',loc2glob_array)
 
     def w(x):
         return np.where(loc2glob_array == x)[0][0]
@@ -58,8 +64,9 @@ if __name__ == '__main__':
 
     coord_som_loc = msh.vertices[verts]
     el2vert_loc = np.vectorize(w)(msh.elt2verts[list(el_subdomain)])
+    ##print("coord_som_loc ===",coord_som_loc)
     visu_split_mesh.view(coord_som_loc, el2vert_loc, 1,
-                         np.zeros(len(verts)), title='Partition par éléments ' + str(rank))
+                         np.zeros(len(verts)), indInterfNodes=interf_loc, title='Partition par éléments ' + str(rank))
 
     import mesh
     import fem
@@ -80,7 +87,9 @@ if __name__ == '__main__':
 
 
     m = mesh.Mesh(coord_som_loc, el2vert_loc)
+    print("cl av======", cl)
     cl = cl[loc2glob_array]
+    print("cl ap======", cl)
     coords = m.vertices
     elt2verts = m.elt2verts
     nbVerts = coords.shape[0]
@@ -126,8 +135,22 @@ if __name__ == '__main__':
                     for ptCol in range(begRows[iRow], begRows[iRow + 1]):
                         if indCols[ptCol] == iVert:
                             spCoefs[ptCol] = 0.
-
             b[iVert] = f[iVert]
+
+    comm.send(b[interf_loc], dest=(rank + 1) % 2, tag=1)
+    comm.send(interf_one2one,dest=(rank + 1) % 2, tag=0)
+    b_interf = comm.recv(source=(rank + 1) % 2, tag=1)
+    numglob_interf_other_proc = comm.recv(source=(rank + 1) % 2, tag=0)
+    numloc_interf_other_proc = np.in1d(verts, list(numglob_interf_other_proc[0])).nonzero()[0]
+    for i in interf_loc:
+        j = numloc_interf_other_proc.index(i)
+        b[i] += b_interf[j]
+
+    for iVert in range(nbVerts):
+        if (cl[iVert] > 0) and iVert in interf_loc :
+            b[iVert] = f[iVert]
+
+
     # On definit ensuite la matrice :
     spMatrix = sparse.csc_matrix((spCoefs, indCols, begRows),
                                  shape=(nbVerts, nbVerts))
